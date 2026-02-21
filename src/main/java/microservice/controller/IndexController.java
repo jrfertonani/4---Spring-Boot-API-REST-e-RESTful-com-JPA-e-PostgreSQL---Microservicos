c335cfeafd991ccd9d6720162971ba933401aa63
Ademir <ademirjuniorfertonani@gmail.com>
2026-02-21 12:14:44 -0300

    testes

--- src/main/java/microservice/controller/IndexController.java
+++ src/main/java/microservice/controller/IndexController.java
@@ -5,14 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/usuario")
 public class IndexController {
 
     @GetMapping(value = "/")
-    public ResponseEntity init(){
-        return new ResponseEntity("Begin !", HttpStatus.OK);
+    public ResponseEntity init(@RequestParam(value = "nome", required= true, defaultValue = "Nome não informado!") String nome){
+        System.out.println("Parametro sendo recebido!" + nome);
+        return new ResponseEntity("Begin !" + nome, HttpStatus.OK);
     }
 
 
